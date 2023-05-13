3c3190697f56564db444328263f6be8cbc6f5a36
Lakello <59009343+Lakello@users.noreply.github.com>
2023-05-13 14:33:54 +0700

    spawner

--- Assets/Source/Scripts/Enemy/Enemy.cs
+++ Assets/Source/Scripts/Enemy/Enemy.cs
@@ -6,11 +6,12 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int _health;
     [SerializeField] private int _reward;
 
-    [SerializeField] private Player _target;
+    private Player _target;
 
     public Player Target => _target;
+    public int Reward => _reward;
 
-    public event UnityAction Dying;
+    public event UnityAction<Enemy> Dying;
 
     public void Init(Player target)
     {
@@ -23,6 +24,7 @@ public class Enemy : MonoBehaviour
 
         if (_health <= 0)
         {
+            Dying.Invoke(this);
             Destroy(gameObject);
         }
     }
